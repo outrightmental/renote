b2cb263a4324d5461be09eacbcb13dcd69f5f0fd
Nick Kaye <nick.c.kaye@gmail.com>
2014-09-24 17:14:38 -0400

    Renote CLI

--- lib/renote.rb
+++ lib/renote.rb
@@ -1,2 +1,12 @@
 require "renote/version"
 require "renote/cli/application"
+
+module Renote
+  module Cli
+    puts '                   _         '
+    puts '   ___ ___ ___ ___| |_ ___   '
+    puts '  |  _| -_|   | . |  _| -_|  '
+    puts '  |_| |___|_|_|___|_| |___|  '
+    puts '                             '
+  end
+end
